9c3c7618579c494f6dad0196113e6f38a6d3ecc6
Will <william.norris2909@gmail.com>
2026-04-06 15:05:25 +0100

    Fully implemented hashing tools functionality

--- tools/hash_tools.py
+++ tools/hash_tools.py
@@ -1,4 +1,4 @@
-
+import hashlib
 
 def hash_option_list():
     print('Please pick one of the following:\n0: Return to main menu\n1: Hash a string\n2: Hash a file\n3: compare two hashes.')
@@ -40,14 +40,14 @@ def hash_menu():
 
         if choice == 1:
             print('You have chosen to hash a string')
-            continue
+            print(hash_string())
         elif choice == 2:
             print('You have chosen to hash a file')
-            continue
+            print(hash_file())
 
         elif choice == 3:
             print('You have chosen to compare two hashes')
-            continue
+            compare_hashes()
 
         elif choice == 0:
             print('Exiting to main menu...')
@@ -57,6 +57,121 @@ def hash_menu():
             print('Please enter a valid number')
             continue
 
+
+def pick_algorithm():
+
+    print('Here are your algorithm choices:\n1: md5\n2: sha1\n3:sha256\n')
+
     
 
+    while True:
+        alg_choice = input('please enter a number\n')
+        try:
+            choice = int(alg_choice)
+
+        except:
+            print('Please enter a number')
+            continue
+
+        if choice == 1:
+            print('You have picked md5')
+            return 'md5'
+        elif choice == 2:
+            print('You have picked sha1')
+            return 'sha1'
+        elif choice == 3:
+            print('You have picked sha256')
+            return 'sha256'
+        else:
+            print('Enter a valid number')
+            continue
+
+
+    
+
+
+def hash_string():
+
+    pre_string = input('Please enter a string you would like to hash\n')
+
+    alg = pick_algorithm()
+
+    if alg == 'md5':
+        post_string = hashlib.md5(pre_string.encode()).hexdigest()
+        
+
+    elif alg == 'sha1':
+        post_string = hashlib.sha1(pre_string.encode()).hexdigest()
+        
+
+    elif alg == 'sha256':
+        post_string = hashlib.sha256(pre_string.encode()).hexdigest()
+    
+    
+
+    return post_string
+
+
+def hash_file():
+
+    pre_file = input('enter a filename to be hashed\n')
+
+    alg = pick_algorithm()
+
+    try:
+        with open(pre_file, 'rb') as f:
+            contents = f.read()
+            if alg == 'md5':
+                post_file = hashlib.md5(contents).hexdigest()
+
+            elif alg =='sha1':
+                post_file = hashlib.sha1(contents).hexdigest()
+
+            elif alg == 'sha256':
+                post_file = hashlib.sha256(contents).hexdigest()
+
+            
+            return post_file
+
+
+    except:
+        print('There was an error')
+
+
+def compare_hashes():
+
+    file_or_string = input('Would you like to  compare string hashes or  compare file hashes (file/string)\n')
+
+   
+
+    if file_or_string == 'file':
+        unknown_hash = hash_file()
+       
+        
+
+    elif file_or_string == 'string' :
+       unknown_hash = hash_string()
+
+    else:
+        print('Enter a valid choice')
+        return 'error'
+    
+
+    known_hash = input('Enter the known hash\n')
+       
+      
+
+    if unknown_hash == known_hash:
+            print('They match')
+
+    else:
+            print('They do not')
+
+
+
+    
+    
+    
+
+    
 
